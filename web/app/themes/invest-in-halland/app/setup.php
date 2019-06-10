2d4ef11b1dd68f5211e67c82033bd12b2c173015
David Öhlin <hello@davidohl.in>
2019-06-10 15:29:28 +0200

    add article, question and news custom post type :custard:

--- web/app/themes/invest-in-halland/app/setup.php
+++ web/app/themes/invest-in-halland/app/setup.php
@@ -138,3 +138,148 @@ add_action('admin_menu', function() {
     // Hide Posts
     remove_menu_page('edit.php');
 });
+
+/**
+ * Custom Post Type: Article
+ */
+add_action('init', function() {
+    $labels = array(
+        'name'                  => _x( 'Artiklar', 'Post Type General Name', 'investinhalland' ),
+        'singular_name'         => _x( 'Artikel', 'Post Type Singular Name', 'investinhalland' ),
+        'menu_name'             => __( 'Artiklar', 'investinhalland' ),
+        'name_admin_bar'        => __( 'Artiklar', 'investinhalland' ),
+        'parent_item_colon'     => __( 'Parent Item:', 'investinhalland' ),
+        'search_items'          => __( 'Search Item', 'investinhalland' ),
+        'items_list'            => __( 'Items list', 'investinhalland' ),
+        'items_list_navigation' => __( 'Items list navigation', 'investinhalland' ),
+        'filter_items_list'     => __( 'Filter items list', 'investinhalland' ),
+    );
+
+    $rewrite = array(
+        'slug'                  => 'artiklar',
+        'with_front'            => true,
+        'pages'                 => true,
+        'feeds'                 => true,
+    );
+    
+    $args = array(
+        'label'                 => __( 'Article', 'investinhalland' ),
+        'description'           => __( 'Articles', 'investinhalland' ),
+        'labels'                => $labels,
+        'supports'              => array( 'title', 'editor', 'thumbnail', 'author' ),
+        'hierarchical'          => false,
+        'public'                => true,
+        'show_ui'               => true,
+        'show_in_menu'          => true,
+        'menu_icon'             => 'dashicons-format-quote',
+        'menu_position'         => 5,
+        'show_in_admin_bar'     => true,
+        'show_in_nav_menus'     => true,
+        'can_export'            => true,
+        'has_archive'           => true,
+        'exclude_from_search'   => false,
+        'publicly_queryable'    => true,
+        'capability_type'       => 'page',
+        'show_in_rest'          => true,
+        'rewrite'               => $rewrite,
+    );
+    
+    register_post_type( 'article', $args );
+}, 0);
+
+/**
+ * Custom Post Type: Frågor
+ */
+add_action('init', function() {
+    $labels = array(
+        'name'                  => _x( 'Frågor', 'Post Type General Name', 'investinhalland' ),
+        'singular_name'         => _x( 'Fråga', 'Post Type Singular Name', 'investinhalland' ),
+        'menu_name'             => __( 'Frågor', 'investinhalland' ),
+        'name_admin_bar'        => __( 'Frågor', 'investinhalland' ),
+        'parent_item_colon'     => __( 'Parent Item:', 'investinhalland' ),
+        'search_items'          => __( 'Search Item', 'investinhalland' ),
+        'items_list'            => __( 'Items list', 'investinhalland' ),
+        'items_list_navigation' => __( 'Items list navigation', 'investinhalland' ),
+        'filter_items_list'     => __( 'Filter items list', 'investinhalland' ),
+    );
+
+    $rewrite = array(
+        'slug'                  => 'fragor',
+        'with_front'            => true,
+        'pages'                 => true,
+        'feeds'                 => true,
+    );
+    
+    $args = array(
+        'label'                 => __( 'Question', 'investinhalland' ),
+        'description'           => __( 'Questions', 'investinhalland' ),
+        'labels'                => $labels,
+        'supports'              => array( 'title', 'editor', 'thumbnail', 'author' ),
+        'hierarchical'          => false,
+        'public'                => true,
+        'show_ui'               => true,
+        'show_in_menu'          => true,
+        'menu_icon'             => 'dashicons-chart-bar',
+        'menu_position'         => 5,
+        'show_in_admin_bar'     => true,
+        'show_in_nav_menus'     => true,
+        'can_export'            => true,
+        'has_archive'           => true,
+        'exclude_from_search'   => false,
+        'publicly_queryable'    => true,
+        'capability_type'       => 'page',
+        'show_in_rest'          => true,
+        'rewrite'               => $rewrite,
+    );
+    
+    register_post_type( 'question', $args );
+}, 0);
+
+/**
+ * Custom Post Type: Nyheter
+ */
+add_action('init', function() {
+    $labels = array(
+        'name'                  => _x( 'Nyheter', 'Post Type General Name', 'investinhalland' ),
+        'singular_name'         => _x( 'Nyhet', 'Post Type Singular Name', 'investinhalland' ),
+        'menu_name'             => __( 'Nyheter', 'investinhalland' ),
+        'name_admin_bar'        => __( 'Nyheter', 'investinhalland' ),
+        'parent_item_colon'     => __( 'Parent Item:', 'investinhalland' ),
+        'search_items'          => __( 'Search Item', 'investinhalland' ),
+        'items_list'            => __( 'Items list', 'investinhalland' ),
+        'items_list_navigation' => __( 'Items list navigation', 'investinhalland' ),
+        'filter_items_list'     => __( 'Filter items list', 'investinhalland' ),
+    );
+    $rewrite = array(
+        'slug'                  => 'nyheter',
+        'with_front'            => true,
+        'pages'                 => true,
+        'feeds'                 => true,
+    );
+    
+    $args = array(
+        'label'                 => __( 'News', 'investinhalland' ),
+        'description'           => __( 'News', 'investinhalland' ),
+        'labels'                => $labels,
+        'supports'              => array( 'title', 'editor', 'thumbnail', 'author'),
+        'taxonomies'            => array( 'area' ),
+        'hierarchical'          => false,
+        'public'                => true,
+        'show_ui'               => true,
+        'show_in_menu'          => true,
+        'menu_icon'             => 'dashicons-rss',
+        'menu_position'         => 5,
+        'show_in_admin_bar'     => true,
+        'show_in_nav_menus'     => true,
+        'show_in_nav_rest'      => true,
+        'can_export'            => true,
+        'has_archive'           => true,
+        'exclude_from_search'   => false,
+        'publicly_queryable'    => true,
+        'capability_type'       => 'page',
+        'show_in_rest'          => true,
+        'rewrite'               => $rewrite
+    );
+    
+    register_post_type( 'news', $args );
+}, 0);
